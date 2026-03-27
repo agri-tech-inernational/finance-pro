418e46142d1e198725c761b32b6d908ba66d4d29
Ahmed Farid <ahmedfarid@macbookpro.localdomain>
2026-03-27 21:14:40 +0100

    Updates

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Basic Service Worker for AgriTech Factory App
+const CACHE_NAME = 'agritech-factory-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
